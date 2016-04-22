576e24dff9db343b138637a76f63f9ed6f826efa
Adam Borowski <aborowski@atlassian.com>
2016-04-22 22:22:30 +0200

    initial import + bib2xml to convert bibliography into xml format + citations migrated + thesis content changed a bit

---
+++ html/src/script/main.js
@@ -0,0 +1,6 @@
+//"use_strict"
+//class Super {
+//    constructor() {
+//        Log.log("dziala ES6")
+//    }
+//}
---
+++ html/tools/bib2xml.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+process.stdin.setEncoding('utf8');
+var chunks = [];
+process.stdin.on('readable', () => {
+    var chunk = process.stdin.read();
+    chunks.push(chunk);
+});
+
+process.stdin.on('end', () => {
+    processBib(chunks.join(''));
+});
+
+function processBib(plain) {
+
+    var result = plain
+        .replace(/@(.*){(.*),/gm, '<citation type="$1" id="$2">') //citation headers
+        .replace(/^}/gm, '</citation>') //enclosing tag
+        .replace(/howpublished\s*=\s*{"\\url{(.*)}"}/gm, "url = {$1}") // howpublished into url
+        .replace(/(\w*)\s*=\s*\{(.*)},?$/gm, '<$1>$2</$1>') // attributes into tags
+        .replace(/\s*#(.*)$/gm, '\n<!-- $1 -->') // commentation syntax
+        ;
+
+    process.stdout.write(result);
+}
