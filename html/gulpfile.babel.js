42eddea5c6dece0a7a20f2cb8b6b0d7c3cfeeb17
Adam Borowski <aborowski@atlassian.com>
2016-04-23 16:41:39 +0200

    safe copy

--- html/gulpfile.babel.js
+++ html/gulpfile.babel.js
@@ -23,14 +23,14 @@ gulp.task('-compile-js', () => {
 gulp.task('-compile-less', () => {
     return gulp.src('src/stylesheets/main.less')
         .pipe(less({
-            relativeUrls: true
+            relativeUrls: false
         }))
         .pipe(gulp.dest('target'));
 });
 
 
 gulp.task('-prince',
-    shell.task('prince src/content/main.html -o target/Thesis.pdf --javascript --script=node_modules/jquery/jquery.js  --script=target/main.js  -s target/main.css --fileroot=src/content/resources', {})
+    shell.task('prince src/content/main.html -o target/Thesis.pdf --javascript --script=node_modules/jquery/jquery.js  --script=target/main.js  -s target/main.css --fileroot=src/content/resources/', {})
 );
 
 gulp.task('default', ['-compile-js', '-compile-less', '-prince'], ()=> {
@@ -41,6 +41,6 @@ gulp.task('default', ['-compile-js', '-compile-less', '-prince'], ()=> {
 gulp.task('watch', ['default'], ()=> {
     gulp.watch('src/scripts/**/*.js', ['-compile-js']);
     gulp.watch('src/stylesheets/**/*.less', ['-compile-less']);
-    gulp.watch('src/content/**/*.html', ['-prince']);
+    gulp.watch('src/content/**', ['-prince']);
     gulp.watch(['target/main.js', 'target/main.css'], ['-prince']);
 });
