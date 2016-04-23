5f4988eba8b528fae70cb4fc94172d550e9d32df
Adam Borowski <aborowski@atlassian.com>
2016-04-23 14:27:28 +0200

    simple framework for thesis

---
+++ html/target/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var _TOCGenerator = require('TOCGenerator');
+
+var _TOCGenerator2 = _interopRequireDefault(_TOCGenerator);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+$('h1').html('hacked');
+console.log('what??s');
+Log.log('dupa');
