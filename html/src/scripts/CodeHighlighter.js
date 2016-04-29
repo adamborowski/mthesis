86c3e879fc9cac7f084caa5d2a59074b6d9795da
Adam Borowski <aborowski@atlassian.com>
2016-04-29 11:45:22 +0200

    added translation formatting removed PRE surrounding CODE due to changed IDEA settings to not autoformat CODE element

--- html/src/scripts/CodeHighlighter.js
+++ html/src/scripts/CodeHighlighter.js
@@ -1,9 +1,24 @@
 export default class CodeHighlighter {
     constructor() {
-        // $('figure[type="code"] code').each((i, block)=> {
-        //     block.innerHTML = block.innerHTML.trim();
-        //     hljs.highlightBlock(block);
-        // });
-        hljs.initHighlighting();
+        $('figure[type="code"] code').each((i, $code)=> {
+            this.correctWhitespacesForCode($code);
+            hljs.highlightBlock($code);
+        });
+    }
+
+    correctWhitespacesForCode($code) {
+        var lines = $code.textContent.split('\n');
+        if (lines[0] === '') {
+            lines.shift()
+        }
+        var matches;
+        var indentation = (matches = /^[\s\t]+/.exec(lines[0])) !== null ? matches[0] : null;
+        if (!!indentation) {
+            lines = lines.map(function (line) {
+                line = line.replace(indentation, '')
+                return line.replace(/\t/g, '    ')
+            });
+            $code.textContent = lines.join('\n').trim();
+        }
     }
 }
