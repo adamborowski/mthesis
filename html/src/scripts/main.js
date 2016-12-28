74c55949a5f0b6ea59ba394dcda37482228af772
Adam Borowski <aborowski@atlassian.com>
2016-12-28 03:25:00 +0100

    READY CV

--- html/src/scripts/main.js
+++ html/src/scripts/main.js
@@ -5,4 +5,17 @@ $(window).on('load', ()=> {
     //     e.clone().appendTo(e.parent());
     // });
 
+    $('[data-from]').each((id, elem) => {
+        let $el = $(elem);
+        $el.html(`since ${$el.attr('data-from').replace('-', '/')}`);
+    });
+    $('[data-to]').each((id, elem) => {
+        let $el = $(elem);
+        $el.html($el.attr('data-to').replace('-', '/'));
+    });
+    $('[data-to][data-from]').each((id, elem) => {
+        let $el = $(elem);
+        $el.html(`${$el.attr('data-from').replace('-', '/')} &dash; ${$el.attr('data-to').replace('-', '/')}`);
+    });
+
 });
